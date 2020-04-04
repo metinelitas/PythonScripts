9ef0e64d1ffc50981e6bcf2aef07c6eeeb06170e
Metin Elitas <metinelitas@gmial.com>
2020-04-04 14:10:53 +0300

    SonyA6000

---
+++ backupA6000.py
@@ -0,0 +1,60 @@
+import os
+import time
+import pathlib
+import exifread
+import glob
+import shutil
+
+
+# cameraPath = "C:\\Users\\metin\\Desktop\\import\\"
+cameraPath = "F:\\"
+outputPath = "C:\\Users\\metin\\Desktop\\output\\"
+
+
+def copyFileToDirectory(directoryName,fileName):
+    directoryToCopy = os.path.join(outputPath,directoryName)
+    # print("dir to copy:" + directoryToCopy)
+    if os.path.exists(directoryToCopy) != 1:
+        print("Create dir: " + directoryToCopy)
+        os.mkdir(directoryToCopy)
+
+    print("Copying " +  fileName + " --->" + directoryToCopy)
+    shutil.copy(fileName, directoryToCopy)
+    
+
+def copyMtsFiles():
+    for r, d, f in os.walk(cameraPath):
+        for file in f:
+            if file.endswith(".MTS"):
+                fileFullPath = os.path.join(r, file) 
+                modifiedTime = time.gmtime(os.path.getmtime(fileFullPath))
+                imageTakenDay = modifiedTime.tm_mday.__str__()
+                imageTakenMonth = modifiedTime.tm_mon.__str__()
+                imageTakenYear = modifiedTime.tm_year.__str__()
+                if (len(imageTakenMonth) == 1):
+                    imageTakenMonth = "0"+imageTakenMonth
+                if (len(imageTakenDay) == 1):
+                    imageTakenDay = "0"+imageTakenDay
+                directoryName = imageTakenYear+"-"+imageTakenMonth+"-"+imageTakenDay
+                copyFileToDirectory(directoryName,fileFullPath)
+
+def copyImageFiles():
+    for r, d, f in os.walk(cameraPath):
+        for file in f:
+            if file.endswith(".JPG"):
+                fileFullPath = os.path.join(r, file) 
+                # print(fileFullPath)
+                f = open(fileFullPath, 'rb')
+                exifTags = exifread.process_file(f,details=False)
+                imageDateTimeValue = exifTags['Image DateTime']
+                splittedDateTimeData = str(imageDateTimeValue).split(":")
+                imageTakenDay = splittedDateTimeData[2].split(" ")[0]
+                imageTakenMonth = splittedDateTimeData[1]
+                imageTakenYear = splittedDateTimeData[0]
+                directoryName = imageTakenYear+"-"+imageTakenMonth+"-"+imageTakenDay
+                copyFileToDirectory(directoryName,fileFullPath)
+
+
+copyImageFiles()
+copyMtsFiles()
+
